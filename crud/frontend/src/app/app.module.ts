15dbe8a0cd8a81db33ed6b5f24fb3e13ab6ff10b
Lucas Correa <correadev@outlook.com>
2024-12-02 15:26:18 -0300

    Criação de component para ler os produtos

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { MatInputModule } from '@angular/material/input';
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
 
   imports: [
