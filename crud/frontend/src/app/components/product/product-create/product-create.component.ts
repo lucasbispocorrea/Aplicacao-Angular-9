3cdb14bb81acd7e6a95571a2a180a10033a97cb5
Lucas Correa <correadev@outlook.com>
2024-12-02 17:07:55 -0300

    Ajuste de Bug no preenchimento do 'id' dos produtos

--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -10,7 +10,8 @@ import { Product } from '../product.model';
 })
 export class ProductCreateComponent implements OnInit {
 
-  product: Product = {
+  product: Product = {   
+    
     name: '',
     price: null
   }
@@ -21,12 +22,21 @@ export class ProductCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  createProduct(): void{
-    this.productService.create(this.product).subscribe(() => {
-      this.productService.showMessage('Produto Criado!')
-      this.router.navigate(['/products'])
-    })
+  createProduct(): void {
+    // Primeiro, obtenha todos os produtos para encontrar o maior ID
+    this.productService.read().subscribe(products => {
+      // Calcula o maior ID existente ou usa 0 se não houver produtos
+      const maxId = products.length > 0 ? Math.max(...products.map(p => p.id || 0)) : 0;
+      
+      // Define o novo ID sequencial
+      this.product.id = maxId + 1;
 
+      // Envia o produto com o novo ID
+      this.productService.create(this.product).subscribe(() => {
+        this.productService.showMessage('Produto Criado!');
+        this.router.navigate(['/products']);
+      });
+    });
   }
 
   cancel(): void{
