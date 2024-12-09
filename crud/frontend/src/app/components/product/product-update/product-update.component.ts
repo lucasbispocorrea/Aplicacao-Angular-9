5f3882e77accbc635c3465d4221474b1614cc465
Lucas Correa <correadev@outlook.com>
2024-12-09 06:46:17 -0300

    configuração das rotas de update

--- crud/frontend/src/app/components/product/product-update/product-update.component.ts
+++ crud/frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Product } from "../product.model";
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: "app-product-update",
+  templateUrl: "./product-update.component.html",
+  styleUrls: ["./product-update.component.css"],
 })
 export class ProductUpdateComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {}
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
   }
 
 }
