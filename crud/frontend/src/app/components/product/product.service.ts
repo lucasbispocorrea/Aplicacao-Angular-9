de5e420aaf75da8c54042481541489e0379036ef
Lucas Correa <correadev@outlook.com>
2024-12-09 06:24:24 -0300

    criação dos serviços de Update de ReadById

--- crud/frontend/src/app/components/product/product.service.ts
+++ crud/frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Product } from './product.model'
+import { Product } from './product.model';
 import { Observable } from 'rxjs';
 
 
@@ -28,5 +28,15 @@ export class ProductService {
 
   read(): Observable<Product[]>{
     return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
   }
 }
